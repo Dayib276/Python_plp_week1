04610b968bdf74c789cc62f9fda4eb9cb6473f2b
Dayib276 <dayibidris@gmail.com>
2025-09-11 18:10:53 +0300

    My react basics

---
+++ Desktop/SEP/React/plp_week3/src/components/Task.jsx
@@ -0,0 +1,16 @@
+export default function Task({ title, id, completed, onToggle }) {
+    return (
+         <div className="flex items-center justify-between p-4 bg-white rounded shadow mb-8">
+            <span className={completed ? "line-through text-red-500" : ""}>
+                {title}
+            </span>
+
+            <button className={`px-3 py-1 rounded ${completed ? "bg-green-200" : "bg-blue-200"}`} onClick={()=> onToggle(id)}>
+                {completed ? "Undo" : "Done" }
+            </button>
+        </div>
+    )
+
+
+
+}
